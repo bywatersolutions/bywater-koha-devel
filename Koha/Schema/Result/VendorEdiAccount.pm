628d2b7c1f891d89a260f31cc9203e10baf8773e
Kyle M Hall <kyle@bywatersolutions.com>
2015-10-28 07:15:55 -0400

    Update VendorEdiAccount Schema

--- Koha/Schema/Result/VendorEdiAccount.pm
+++ Koha/Schema/Result/VendorEdiAccount.pm
@@ -121,34 +121,22 @@ __PACKAGE__->table("vendor_edi_accounts");
 =head2 order_file_suffix
 
   data_type: 'varchar'
-<<<<<<< HEAD
   default_value: 'CEP'
   is_nullable: 0
-=======
-  is_nullable: 1
->>>>>>> 0a334ba5cd3af50abac4b2f24eef7e9551c3f750
   size: 3
 
 =head2 quote_file_suffix
 
   data_type: 'varchar'
-<<<<<<< HEAD
   default_value: 'CEQ'
   is_nullable: 0
-=======
-  is_nullable: 1
->>>>>>> 0a334ba5cd3af50abac4b2f24eef7e9551c3f750
   size: 3
 
 =head2 invoice_file_suffix
 
   data_type: 'varchar'
-<<<<<<< HEAD
   default_value: 'CEI'
   is_nullable: 0
-=======
-  is_nullable: 1
->>>>>>> 0a334ba5cd3af50abac4b2f24eef7e9551c3f750
   size: 3
 
 =cut
@@ -264,8 +252,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07040 @ 2015-10-28 06:56:14
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:fOsgH+XGi5K4ToScDxI7Cw
+# Created by DBIx::Class::Schema::Loader v0.07040 @ 2015-10-28 07:15:39
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:7Qp2F62yZO1SruXakH2O5w
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
