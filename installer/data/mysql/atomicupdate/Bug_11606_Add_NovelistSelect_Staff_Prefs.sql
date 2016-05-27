118f4f622a19e3524a77f967f507463668407df7
Nick Clemens <nick@bywatersolutions.com>
2016-05-27 16:51:56 -0400

    Bug 11606 - Add nvelist client to staff
    Work in progress

---
+++ installer/data/mysql/atomicupdate/Bug_11606_Add_NovelistSelect_Staff_Prefs.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type) VALUES ('NovelistSelectStaffEnabled','0',NULL,'Enable  Novelist Select content to the Staff Interface (requires that you have entered in a user profile and password, which can be seen in image links)','YesNo'),
+('NovelistSelectStaffPassword','',NULL,'Novelist Select Staff Profile','free'),
+('NovelistSelectStaffProfile','',NULL,'Novelist Select Staff Password','free'),
+('NovelistSelectStaffView','tab','tab|above|below','Where to display Novelist Select content','Choice');
