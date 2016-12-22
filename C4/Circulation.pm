c84e799f6a9c56eda56c5b91b55dcfd19558ff82
Kyle M Hall <kyle@bywatersolutions.com>
2016-12-22 14:00:37 +0000

    Bug 17781 - Improper branchcode set during renewal
    For no discernable reason, when AddIssue calls AddRenewal, it passes the
    branchcode generated from _GetCircControlBranch. Assume
    _GetCircControlBranch is set to return items.homebranch. So:
    1) If an item owned by LibraryA is checked out at LibraryB, the
      statistic line branchcode will be LibraryB
    2) If an item is renewed via the ajax datatables renewal function, the
      statistic line branchcode will be LibraryB the
    3) If an item is renewed via scanning the item into the checkout again,
      statistic line branchcode will be *LibraryA*
    
    This is clearly improper behavior. The renewal is taking place at
    LibraryB, so the branchcode passed to AddRenewal should be LibraryB,
    the logged in library. This also jives with the documentation for
    the subroutine.
    
    Test Plan:
    1) Set CircControl to "the library the item is from" aka ( ItemHomeLibrary )
    2) Set HomeOrHoldingBranch to 'The library the items is from" ( aka homebranch )
    3) Create item with homebranch of LibraryA and holdingbranch of LibraryB
    4) Set the logged in library to LibraryB
    4) Check the item out to a patron at LibraryB
    5) Note the statistics line has a branchcode of LibraryB
    6) Check the item out again to trigger a renewal, renew the item
    7) Note the statistic line has a branchcode of LibraryA!
    8) Apply this patch
    9) Repeat step 6
    10) Note the statistics line has a branchcode of LibraryB!
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: David Kuhn <kuhn@monterey.org>

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -3015,15 +3015,19 @@ sub AddRenewal {
     }
 
     # Log the renewal
-    UpdateStats({branch => $branch,
-                type => 'renew',
-                amount => $charge,
-                itemnumber => $itemnumber,
-                itemtype => $item->{itype},
-                borrowernumber => $borrowernumber,
-                ccode => $item->{'ccode'}}
-                );
-	return $datedue;
+    UpdateStats(
+        {
+            branch => C4::Context->userenv ? C4::Context->userenv->{branch} : $branch,
+            type           => 'renew',
+            amount         => $charge,
+            itemnumber     => $itemnumber,
+            itemtype       => $item->{itype},
+            borrowernumber => $borrowernumber,
+            ccode          => $item->{'ccode'}
+        }
+    );
+
+    return $datedue;
 }
 
 sub GetRenewCount {
