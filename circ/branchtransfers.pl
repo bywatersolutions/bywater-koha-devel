0a353ce0fa652f9f9eb7fec030ef80ad060dd7f9
Kyle M Hall <kyle@bywatersolutions.com>
2017-02-21 12:26:25 +0000

    Bug 16735: Remove use of get_categories
    Feature using it is completely undocumented as far as my research has
    shown.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- circ/branchtransfers.pl
+++ circ/branchtransfers.pl
@@ -206,10 +206,6 @@ foreach my $code ( keys %$messages ) {
             $err{tbr}      = $tbr;
             $err{code}     = $typecode;
         }
-        elsif ( $code eq 'IsPermanent' ) {
-            $err{errispermanent} = 1;
-            $err{msg} = $messages->{'IsPermanent'};
-        }
         elsif ( $code eq 'WasReturned' ) {
             $err{errwasreturned} = 1;
             $err{borrowernumber} = $messages->{'WasReturned'};
