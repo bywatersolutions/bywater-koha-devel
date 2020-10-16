3b50d9df167e0093da9a0666d4aa46924ce168c8
Kyle M Hall <kyle@bywatersolutions.com>
2020-10-16 10:42:35 -0400

    Bug 26351: (QA follow-up) Barcodes inputted into Koha should always pass though barcodedecode
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- C4/ILSDI/Services.pm
+++ C4/ILSDI/Services.pm
@@ -642,8 +642,7 @@ sub GetServices {
 
     # Issuing management
     my $barcode = $item->barcode || '';
-    $barcode = barcodedecode($barcode) if ( $barcode && C4::Context->preference('itemBarcodeInputFilter') );
-    ($barcode) = Koha::Plugins->call('item_barcode_transform', $barcode ) || $barcode;
+    $barcode = barcodedecode($barcode) if $barcode;
     if ($barcode) {
         my ( $issuingimpossible, $needsconfirmation ) = CanBookBeIssued( $patron, $barcode );
 
