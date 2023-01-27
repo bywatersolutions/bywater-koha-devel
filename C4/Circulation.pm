2ef0803d5f0496cd7fde36d611f4123b54c6c69b
Kyle Hall <kyle@bywatersolutions.com>
2023-01-27 09:22:59 -0500

    Bug 29145: (QA follow-up) Rename has_debarring_overdues to has_restricting_overdues

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -2562,7 +2562,7 @@ sub MarkIssueReturned {
         if (C4::Context->preference('AutoRemoveOverduesRestrictions') ne 'no' && $patron->is_debarred) {
             my $remove_restrictions =
                 C4::Context->preference('AutoRemoveOverduesRestrictions') eq 'when_no_overdue_causing_debarment' ?
-                    !$patron->has_debarring_overdues({ issue_branchcode => $issue_branchcode }) :
+                    !$patron->has_restricting_overdues({ issue_branchcode => $issue_branchcode }) :
                     !$patron->has_overdues;
             if (
                 $remove_restrictions &&
