c84de245e3f6b721433d4c49b87836334908c1b0
Michael Hafen <michael.hafen@washk12.org>
2024-09-19 12:26:30 -0600

    Bug 37966: When Renewing a book on hold, don't require the due date
    When renewing a book on hold with the RenewalOnHoldOverride sys pref set
    to "Allow", if the due date is not specified then it becomes "now".  By
    checking the input is set rather than just defined the due date is set as
    expected based on circulation rules.
    Also change the description of the RenewalOnHoldOverride sys pref as the
    due date would no longer be required to be entered with this change.
    
    Test plan:
    1. make sure RenewalOnHoldOverride is set to 'Allow'
    2. check out a book, then place a title-level hold on it
    3. renew the book, see the hold override dialog
    4. leave the due date empty and hit the override button
    5. observe the new due date is "now"
    6. apply patch and restart plack
    7. renew the book again, overriding the hold, and see the expected due date
       as per circulation rules.
    
    Signed-off-by: Marion Durand <marion.durand@biblibre.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -3552,7 +3552,7 @@ sub AddRenewal {
 
     $borrowernumber ||= $issue->borrowernumber;
 
-    if ( defined $datedue && ref $datedue ne 'DateTime' ) {
+    if ( $datedue && ref $datedue ne 'DateTime' ) {
         $datedue = dt_from_string( $datedue, 'sql' );
     }
 
