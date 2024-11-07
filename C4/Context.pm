319552bde33fe49813cc4894508cde60408ac326
Kyle M Hall <kyle@bywatersolutions.com>
2024-11-07 06:53:39 -0500

    Bug 38384: Trigger plugin modules loading as soon as possible in Koha
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomás Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Andrew Fuerste Henry <andrew@bywatersolutions.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit 654556247feb2611c5eb51cae6f91141db0e0103)
    Signed-off-by: Jacob O'Mara <Jacob.omara@openfifth.co.uk>

--- C4/Context.pm
+++ C4/Context.pm
@@ -22,7 +22,12 @@ use Modern::Perl;
 use vars qw($AUTOLOAD $context);
 
 BEGIN {
-    if ( $ENV{'HTTP_USER_AGENT'} ) {    # Only hit when plack is not enabled
+    # Calling get_enabled_plugins here esnures that all plugin
+    # modules are loaded before use and will not trigger
+    # a database connection reset
+    Koha::Plugins->get_enabled_plugins();
+
+    if ( $ENV{'HTTP_USER_AGENT'} ) { # Only hit when plack is not enabled
 
         # Redefine multi_param if cgi version is < 4.08
         # Remove the "CGI::param called in list context" warning in this case
