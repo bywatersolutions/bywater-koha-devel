adcbd749e4c73a86bbd83e428bec76340516fc63
Aleisha Amohia <aleisha@catalyst.net.nz>
2025-12-17 01:36:35 +0000

    Bug 41462: Add Koha::OAI::Set(s) and Koha::OAI::Set::Biblio(s)
    This architectural changes makes these tables available to the Koha namespace, and therefore the REST APIs.
    
    To test: Confirm t/00-load.t tests pass and check for any syntax/tidy errors.
    
    Sponsored-by: Auckland University of Technology
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>

---
+++ Koha/OAI/Set.pm
@@ -0,0 +1,46 @@
+package Koha::OAI::Set;
+
+# Copyright 2025 Aleisha Amohia <aleisha@catalyst.net.nz>
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <https://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Koha::Database;
+
+use base qw(Koha::Object);
+
+=head1 NAME
+
+Koha::OAI::Set - Koha OaiSet Object class
+
+=head1 API
+
+=head2 Class methods
+
+=cut
+
+=head2 Internal methods
+
+=head3 _type
+
+=cut
+
+sub _type {
+    return 'OaiSet';
+}
+
+1;
