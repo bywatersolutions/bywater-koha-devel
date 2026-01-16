c5a34818de9077a648b7a95aeb6f1230cd655a3c
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2026-01-16 16:16:46 +0100

    Bug 41653: Move addPrefs and addPermissions under the Koha namespace
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- koha-tmpl/opac-tmpl/bootstrap/js/stores.js
+++ koha-tmpl/opac-tmpl/bootstrap/js/stores.js
@@ -1,19 +1,17 @@
 (function () {
-    const permissions = (window.permissions = window.permissions || {});
+    const app = (window.Koha ??= {});
 
-    window.addPermissions = function (perms) {
+    const permissions = (app.permissions ??= {});
+    app.addPermissions = function (perms) {
         for (const key in perms) {
             if (Object.prototype.hasOwnProperty.call(perms, key)) {
                 permissions[key] = perms[key];
             }
         }
     };
-})();
 
-(function () {
-    const prefs = (window.prefs = window.prefs || {});
-
-    window.addPrefs = function (sysprefs) {
+    const prefs = (app.prefs = app.prefs || {});
+    app.addPrefs = function (sysprefs) {
         for (const key in sysprefs) {
             if (Object.prototype.hasOwnProperty.call(sysprefs, key)) {
                 prefs[key] = sysprefs[key];
