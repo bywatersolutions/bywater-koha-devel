8948d7117ad6fc2fe1532ff722b7734774c04343
Katrin Fischer <katrin.fischer@bsz-bw.de>
2026-02-20 14:59:00 +0000

    Bug 41814: (QA follow-up) Add bug number to atomic update and some style fixes
    Sponsored-by: Cheshire West and Chester Council
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>

--- installer/data/mysql/atomicupdate/bug_xxxxx.pl
+++ installer/data/mysql/atomicupdate/bug_xxxxx.pl
@@ -2,8 +2,8 @@ use Modern::Perl;
 use Koha::Installer::Output qw(say_warning say_success say_info);
 
 return {
-    bug_number  => "XXXXX",
-    description => "Add a new syspref for PatronAgeRestriction",
+    bug_number  => "41900",
+    description => "Add a new system preference for PatronAgeRestriction",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
@@ -14,13 +14,13 @@ return {
             INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES ('PatronAgeRestriction', '', NULL, 'Patron\'s maximum age during registration. If empty, no age restriction is applied.', 'Integer')
         }
         );
-        say $out "Added new syspref 'PatronAgeRestriction'";
+        say_success( $out, "Added new system preference 'PatronAgeRestriction'" );
 
         $dbh->do(
             q{
             UPDATE IGNORE systempreferences SET `explanation` = 'Patron\'s maximum age during self registration. If empty, honor PatronAgeRestriction.' WHERE `variable` = 'PatronSelfRegistrationAgeRestriction'
         }
         );
-        say $out "Updated explanation for syspref 'PatronSelfRegistrationAgeRestriction'";
+        say_success( $out, "Updated explanation for system preference 'PatronSelfRegistrationAgeRestriction'" );
     },
 };
