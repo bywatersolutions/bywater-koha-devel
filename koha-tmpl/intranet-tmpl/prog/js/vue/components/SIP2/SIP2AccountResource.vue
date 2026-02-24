b016ae79a019566c7e085adab95845c4f9783a72
Pedro Amorim <pedro.amorim@openfifth.co.uk>
2026-02-24 14:37:20 -0100

    Bug 41916: Clean-up obsolete code
    No longer needed after bug 40191, but because both that and bug 37893 went in close to each other, this remained.
    Removing this fixes the toolbar buttons on the SIP2 account form and fixes the console warning that was causing a failure in cypress tests
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit 6f137822f765e9321fd5e73eb6b279248f9d2982)
    Signed-off-by: Jacob O'Mara <Jacob.omara@openfifth.co.uk>

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/SIP2/SIP2AccountResource.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/SIP2/SIP2AccountResource.vue
@@ -926,25 +926,6 @@ export default {
             },
         ];
 
-        const additionalToolbarButtons = (resource, componentData) => {
-            const buttons = {
-                form: [
-                    {
-                        title: $__("Submit"),
-                        form: componentData.resourceForm,
-                    },
-                    {
-                        to: {
-                            name: "SIP2AccountsList",
-                        },
-                        title: $__("Cancel"),
-                        cssClass: "btn btn-link",
-                    },
-                ],
-            };
-            return buttons;
-        };
-
         const baseResource = useBaseResource({
             resourceName: "account",
             nameAttr: "login_id",
@@ -971,7 +952,6 @@ export default {
                     APIClient.sip2.httpClient._baseURL + "accounts",
                 options: { embed: "institution" },
             },
-            additionalToolbarButtons,
             stickyToolbar: ["Form"],
             embedded: props.embedded,
             formGroupsDisplayMode: "accordion",
