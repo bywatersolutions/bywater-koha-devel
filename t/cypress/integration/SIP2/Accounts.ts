fb921843ab8cd55aed332c039796e71d9a905b60
Pedro Amorim <pedro.amorim@openfifth.co.uk>
2026-02-24 15:38:09 +0000

    Bug 41916: Update Cypress tests
    Button is now 'Save' not 'Submit' after bug 40191
    
    On ktd:
    cypress run --spec t/cypress/integration/SIP2/*
    
    All tests must pass
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- t/cypress/integration/SIP2/Accounts.ts
+++ t/cypress/integration/SIP2/Accounts.ts
@@ -136,7 +136,7 @@ describe("Accounts", () => {
         cy.left_menu_active_item_is("Accounts");
 
         // Fill in the form for normal attributes
-        cy.get("#accounts_add").contains("Submit").click();
+        cy.get("#accounts_add").contains("Save").click();
         cy.get("input:invalid,textarea:invalid,select:invalid").should(
             "have.length",
             2
@@ -195,7 +195,7 @@ describe("Accounts", () => {
 
         // relationshipWidgets
         cy.contains("Add new custom patron field").click();
-        cy.get("#accounts_add").contains("Submit").click();
+        cy.get("#accounts_add").contains("Save").click();
         cy.get("input:invalid,textarea:invalid,select:invalid").should(
             "have.length",
             2
@@ -239,7 +239,7 @@ describe("Accounts", () => {
         cy.intercept("POST", "/api/v1/sip2/accounts", {
             statusCode: 500,
         });
-        cy.get("#accounts_add").contains("Submit").click();
+        cy.get("#accounts_add").contains("Save").click();
 
         cy.get("main div[class='alert alert-warning']").contains(
             "Something went wrong: Error: Internal Server Error"
@@ -250,7 +250,7 @@ describe("Accounts", () => {
             statusCode: 201,
             body: account,
         });
-        cy.get("#accounts_add").contains("Submit").click();
+        cy.get("#accounts_add").contains("Save").click();
         cy.get("main div[class='alert alert-info']").contains(
             "Account created"
         );
@@ -310,7 +310,7 @@ describe("Accounts", () => {
                 delay: 1000,
             });
         }).as("edit-account");
-        cy.get("#accounts_add").contains("Submit").click();
+        cy.get("#accounts_add").contains("Save").click();
         cy.get("main div[class='modal_centered']").contains("Submitting...");
         cy.wait("@edit-account");
         cy.get("main div[class='alert alert-warning']").contains(
@@ -322,7 +322,7 @@ describe("Accounts", () => {
             statusCode: 200,
             body: account,
         });
-        cy.get("#accounts_add").contains("Submit").click();
+        cy.get("#accounts_add").contains("Save").click();
         cy.get("main div[class='alert alert-info']").contains(
             "Account updated"
         );
