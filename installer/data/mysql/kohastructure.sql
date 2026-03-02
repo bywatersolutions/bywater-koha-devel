c3b6df654e99ec3f02c1ad89df7e04fcff90a148
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2026-03-02 12:08:05 +0100

    Bug 40504: Fix DB structure comparison
    Fix misc4dev/run_tests.pl --run-db-compare-only
    
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -3808,16 +3808,16 @@ CREATE TABLE `illrequests` (
   `batch_id` int(11) DEFAULT NULL COMMENT 'Optional ID of batch that this request belongs to',
   PRIMARY KEY (`illrequest_id`),
   KEY `illrequests_bnfk` (`borrowernumber`),
-  KEY `illrequests_manfk` (`managedby`),
   KEY `illrequests_bcfk_2` (`branchcode`),
   KEY `illrequests_safk` (`status_alias`),
   KEY `illrequests_bibfk` (`biblio_id`),
   KEY `illrequests_ibfk` (`batch_id`),
+  KEY `illrequests_manfk` (`managedby`),
   CONSTRAINT `illrequests_bcfk_2` FOREIGN KEY (`branchcode`) REFERENCES `branches` (`branchcode`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `illrequests_bibfk` FOREIGN KEY (`biblio_id`) REFERENCES `biblio` (`biblionumber`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `illrequests_bnfk` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `illrequests_manfk` FOREIGN KEY (`managedby`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `illrequests_ibfk` FOREIGN KEY (`batch_id`) REFERENCES `illbatches` (`ill_batch_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `illrequests_manfk` FOREIGN KEY (`managedby`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `illrequests_safk` FOREIGN KEY (`status_alias`) REFERENCES `authorised_values` (`authorised_value`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
