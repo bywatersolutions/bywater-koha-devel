bdd498af9edb255671d292187d78c0ff850ceefe
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-03-21 07:56:40 +0000

    Bug 41615: (RM follow-up) Fix race condition in Licenses_spec 'List license' test
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>

--- t/cypress/integration/ERM/Licenses_spec.ts
+++ t/cypress/integration/ERM/Licenses_spec.ts
@@ -24,16 +24,15 @@ describe("License CRUD operations", () => {
             headers: {
                 "X-Total-Count": "5",
             },
-        }).as("getDashboardLicenses");
+        });
     });
 
     it("List license", () => {
-        cy.visit("/cgi-bin/koha/erm/erm.pl");
-        cy.wait("@getDashboardLicenses");
         // GET license returns 500
         cy.intercept("GET", "/api/v1/erm/licenses*", {
             statusCode: 500,
         });
+        cy.visit("/cgi-bin/koha/erm/erm.pl");
         cy.get(".sidebar_menu").contains("Licenses").click();
         cy.get("main div[class='alert alert-warning']").contains(
             "Something went wrong: Error: Internal Server Error"
