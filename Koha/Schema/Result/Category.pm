7c7258b9da4469bce0be8b4bcc99de14b243aebc
Lucas Gass <lucas@bywatersolutions.com>
2026-05-01 14:48:35 +0000

    Bug 26355: (RM follow-up) force DBIC
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- Koha/Schema/Result/Category.pm
+++ Koha/Schema/Result/Category.pm
@@ -456,8 +456,8 @@ __PACKAGE__->has_many(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07051 @ 2026-04-14 22:00:11
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Nr9Gjre3v/xBHlm42uNr1g
+# Created by DBIx::Class::Schema::Loader v0.07051 @ 2026-05-01 15:04:38
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:AHEFcNHv+0qmXzr6ETrtDA
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 
