233cf7e7567a470dbf3b305b335a5b626e34e5b0
Pedro Amorim <pedro.amorim@openfifth.co.uk>
2026-05-08 09:30:22 +0000

    Bug 37762: (QA follow-up): Update kohastructure.sql to match atomicupdate
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -3793,11 +3793,12 @@ CREATE TABLE `iso18626_requesting_agencies` (
   `name` varchar(80) DEFAULT NULL COMMENT 'Requesting agency name',
   `borrowernumber` int(11) NOT NULL COMMENT 'foreign key, linking this to the borrowers table (ILL partner patron)',
   `type` enum('DNUCNI','ICOLC','ISIL') NOT NULL COMMENT 'ISO18626 agency type',
-  `account_id` varchar(80) NOT NULL COMMENT 'Authentication: Requesting agency account ID', /* TODO:  MAKE THIS UNIQUE */ 
+  `account_id` varchar(80) NOT NULL COMMENT 'Authentication: Requesting agency account ID',
   `securityCode` varchar(80) NOT NULL COMMENT 'Authentication: Requesting agency security code',
   `callback_endpoint` mediumtext NOT NULL COMMENT 'Callback endpoint to send messages back to',
   PRIMARY KEY (`iso18626_requesting_agency_id`),
   UNIQUE KEY `uniq_borrowernumber` (`borrowernumber`),
+  UNIQUE KEY `uniq_account_id` (`account_id`),
   KEY `iso18626_requesting_agencies_bnfk` (`borrowernumber`),
   CONSTRAINT `iso18626_requesting_agencies_bnfk` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -3817,7 +3818,7 @@ CREATE TABLE `iso18626_requests` (
   `created_on` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Date and time the request was created',
   `updated_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Date and time the request was last updated',
   `requestingAgencyRequestId` varchar(80) DEFAULT NULL COMMENT 'Requesting agency request ID or number',
-  `status` enum('RequestReceived','ExpectToSupply','WillSupply','Loaned', 'Overdue', 'Recalled', 'RetryPossible', 'Unfilled', 'HoldReturn', 'ReleaseHoldReturn', 'CopyCompleted', 'LoanCompleted', 'CompletedWithoutReturn', 'Cancelled') DEFAULT 'RequestReceived' COMMENT 'Current ISO18626 status of request',
+  `status` enum('RequestReceived','ExpectToSupply','WillSupply','Loaned','Overdue','Recalled','RetryPossible','Unfilled','HoldReturn','ReleaseHoldReturn','CopyCompleted','LoanCompleted','CompletedWithoutReturn','Cancelled') DEFAULT 'RequestReceived' COMMENT 'Current ISO18626 status of request',
   `service_type` enum('Copy','Loan','CopyOrLoan') NOT NULL COMMENT 'ISO18626 service type',
   `pending_requesting_agency_action` enum('Cancel','Renew') DEFAULT NULL COMMENT 'ISO18626 Requesting Agency action that requires a manual response (yes or no)',
   `hold_id` int(11) DEFAULT NULL COMMENT 'ID of the hold related to this ISO18626 request',
@@ -3828,10 +3829,10 @@ CREATE TABLE `iso18626_requests` (
   UNIQUE KEY `uniq_issue_id` (`issue_id`),
   KEY `iso18626_bibfk` (`biblio_id`),
   KEY `iso18626_rafk` (`iso18626_requesting_agency_id`),
-  CONSTRAINT `uniq_reserve_id` FOREIGN KEY (`hold_id`) REFERENCES `reserves` (`reserve_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `uniq_issue_id` FOREIGN KEY (`issue_id`) REFERENCES `issues` (`issue_id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `iso18626_bibfk` FOREIGN KEY (`biblio_id`) REFERENCES `biblio` (`biblionumber`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `iso18626_rafk` FOREIGN KEY (`iso18626_requesting_agency_id`) REFERENCES `iso18626_requesting_agencies` (`iso18626_requesting_agency_id`) ON UPDATE CASCADE
+  CONSTRAINT `iso18626_rafk` FOREIGN KEY (`iso18626_requesting_agency_id`) REFERENCES `iso18626_requesting_agencies` (`iso18626_requesting_agency_id`) ON UPDATE CASCADE,
+  CONSTRAINT `uniq_issue_id` FOREIGN KEY (`issue_id`) REFERENCES `issues` (`issue_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `uniq_reserve_id` FOREIGN KEY (`hold_id`) REFERENCES `reserves` (`reserve_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
