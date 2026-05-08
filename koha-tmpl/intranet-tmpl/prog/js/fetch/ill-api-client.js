openfifth/koha

Bug 37762: VueJS ILL Module

koha-tmpl/intranet-tmpl/prog/js/fetch/ill-api-client.js
@@ -1,0 +1,89 @@
+export class ILLAPIClient {
+    constructor(HttpClient) {
+        this.httpClient = new HttpClient({
+            baseURL: "/api/v1/ill/",
+        });
+    }
+
+    get config() {
+        return {
+            get: () => {
+                ILLModule: 1;
+            }, // TODO: Fetch from API
+        };
+    }
+
+    get supplying() {
+        return {
+            get: id =>
+                this.httpClient.get({
+                    endpoint: "iso18626_requests/" + id,
+                    headers: {
+                        "x-koha-embed": "messages",
+                    },
+                }),
+            getAll: (query, params) =>
+                this.httpClient.getAll({
+                    endpoint: "iso18626_requests",
+                    query,
+                    params,
+                }),
+            patch: (iso18626_request, id) =>
+                this.httpClient.patch({
+                    endpoint: "iso18626_requests/" + id,
+                    body: iso18626_request,
+                }),
+            count: (query = {}) =>
+                this.httpClient.count({
+                    endpoint:
+                        "iso18626_requests?" +
+                        new URLSearchParams({
+                            _page: 1,
+                            _per_page: 20,
+                            ...(query && { q: JSON.stringify(query) }),
+                        }),
+                }),
+        };
+    }
+
+    get requesting_agencies() {
+        return {
+            get: id =>
+                this.httpClient.get({
+                    endpoint: "iso18626_requesting_agencies/" + id,
+                }),
+            getAll: (query, params) =>
+                this.httpClient.getAll({
+                    endpoint: "iso18626_requesting_agencies",
+                    query,
+                    params,
+                }),
+            delete: id =>
+                this.httpClient.delete({
+                    endpoint: "iso18626_requesting_agencies/" + id,
+                }),
+            create: iso18626_requesting_agency =>
+                this.httpClient.post({
+                    endpoint: "iso18626_requesting_agencies",
+                    body: iso18626_requesting_agency,
+                }),
+            update: (iso18626_requesting_agency, id) =>
+                this.httpClient.put({
+                    endpoint: "iso18626_requesting_agencies/" + id,
+                    body: iso18626_requesting_agency,
+                }),
+            count: (query = {}) =>
+                this.httpClient.count({
+                    endpoint:
+                        "iso18626_requesting_agencies?" +
+                        new URLSearchParams({
+                            _page: 1,
+                            _per_page: 1,
+                            ...(query && { q: JSON.stringify(query) }),
+                        }),
+                }),
+        };
+    }
+}
+
+export default ILLAPIClient;

koha-tmpl/intranet-tmpl/prog/js/vue/routes/ill.js
@@ -1,0 +1,112 @@
+import { markRaw } from "vue";
+
+import Home from "../components/ILL/Home.vue";
+import ProgressRequest from "../components/ILL/ProgressRequest.vue";
+import ResourceWrapper from "../components/ResourceWrapper.vue";
+
+import { $__ } from "@koha-vue/i18n";
+
+export const routes = [
+    {
+        path: "/cgi-bin/koha/ill/ill.pl",
+        is_default: true,
+        is_base: true,
+        title: $__("Interlibrary loans"),
+        children: [
+            {
+                path: "",
+                name: "Home",
+                component: markRaw(Home),
+                is_navigation_item: false,
+            },
+            {
+                path: "/cgi-bin/koha/ill/ill-requests.pl",
+                title: $__("Requesting ILLs"),
+                icon: "fa fa-download",
+                is_end_node: true,
+            },
+            {
+                path: "/cgi-bin/koha/ill",
+                title: $__("ISO18626"),
+                icon: "fa fa-globe",
+                disabled: true,
+                children: [
+                    {
+                        path: "/cgi-bin/koha/ill/iso18626_requests",
+                        title: $__("Supplying ILLs"),
+                        icon: "fa fa-upload",
+                        is_end_node: true,
+                        resource: "ILL/SupplyingResource.vue",
+                        children: [
+                            {
+                                path: "",
+                                name: "SupplyingList",
+                                component: markRaw(ResourceWrapper),
+                            },
+                            {
+                                path: ":iso18626_request_id",
+                                name: "SupplyingShow",
+                                component: markRaw(ResourceWrapper),
+                                title: "{iso18626_request_id}",
+                            },
+                            {
+                                path: "progress-request/:iso18626_request_id",
+                                name: "ProgressRequest",
+                                component: markRaw(ProgressRequest),
+                                title: $__(
+                                    "Progress request {iso18626_request_id}"
+                                ),
+                            },
+                        ],
+                    },
+                    {
+                        path: "/cgi-bin/koha/ill/iso18626_requesting_agencies",
+                        title: $__("Requesting Agencies"),
+                        icon: "fa fa-building-columns",
+                        is_end_node: true,
+                        resource: "ILL/RequestingAgencyResource.vue",
+                        children: [
+                            {
+                                path: "",
+                                name: "RequestingAgenciesList",
+                                component: markRaw(ResourceWrapper),
+                            },
+                            {
+                                path: ":iso18626_requesting_agency_id",
+                                name: "RequestingAgenciesShow",
+                                component: markRaw(ResourceWrapper),
+                                title: "{name}",
+                            },
+                            {
+                                path: "add",
+                                name: "RequestingAgenciesFormAdd",
+                                component: markRaw(ResourceWrapper),
+                                title: $__("Add requesting agency"),
+                            },
+                            {
+                                path: "edit/:iso18626_requesting_agency_id",
+                                name: "RequestingAgenciesFormAddEdit",
+                                component: markRaw(ResourceWrapper),
+                                title: "{name}",
+                                breadcrumbFormat: ({
+                                    match,
+                                    params,
+                                    query,
+                                }) => {
+                                    match.name = "RequestingAgenciesShow";
+                                    return match;
+                                },
+                                additionalBreadcrumbs: [
+                                    {
+                                        title: $__("Modify requesting agency"),
+                                        disabled: true,
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+];

bywatersolutions/bywater-koha-devel

Signed-off-by: Jason Robb <jrobb@sekls.org>
Signed-off-by: Lisette Scheer <lisette@bywatersolutions.com>
Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

koha-tmpl/intranet-tmpl/prog/js/fetch/ill-api-client.js
@@ -1,0 +1,92 @@
+export class ILLAPIClient {
+    constructor(HttpClient) {
+        this.httpClient = new HttpClient({
+            baseURL: "/api/v1/ill/",
+        });
+    }
+
+    get config() {
+        return {
+            get: () => {
+                ILLModule: 1;
+            }, // TODO: Fetch from API
+        };
+    }
+
+    get supplying() {
+        return {
+            get: id =>
+                this.httpClient.get({
+                    endpoint: "iso18626_requests/" + id,
+                    headers: {
+                        "x-koha-embed": "requesting_agency,messages,hold",
+                    },
+                }),
+            getAll: (query, params) =>
+                this.httpClient.getAll({
+                    endpoint: "iso18626_requests",
+                    query,
+                    params,
+                }),
+            patch: (iso18626_request, id) =>
+                this.httpClient.patch({
+                    endpoint: "iso18626_requests/" + id,
+                    body: iso18626_request,
+                }),
+            count: (query = {}) =>
+                this.httpClient.count({
+                    endpoint:
+                        "iso18626_requests?" +
+                        new URLSearchParams({
+                            _page: 1,
+                            _per_page: 20,
+                            ...(query && { q: JSON.stringify(query) }),
+                        }),
+                }),
+        };
+    }
+
+    get requesting_agencies() {
+        return {
+            get: id =>
+                this.httpClient.get({
+                    endpoint: "iso18626_requesting_agencies/" + id,
+                    headers: {
+                        "x-koha-embed": "ill_partner",
+                    },
+                }),
+            getAll: (query, params) =>
+                this.httpClient.getAll({
+                    endpoint: "iso18626_requesting_agencies",
+                    query,
+                    params,
+                }),
+            delete: id =>
+                this.httpClient.delete({
+                    endpoint: "iso18626_requesting_agencies/" + id,
+                }),
+            create: iso18626_requesting_agency =>
+                this.httpClient.post({
+                    endpoint: "iso18626_requesting_agencies",
+                    body: iso18626_requesting_agency,
+                }),
+            update: (iso18626_requesting_agency, id) =>
+                this.httpClient.put({
+                    endpoint: "iso18626_requesting_agencies/" + id,
+                    body: iso18626_requesting_agency,
+                }),
+            count: (query = {}) =>
+                this.httpClient.count({
+                    endpoint:
+                        "iso18626_requesting_agencies?" +
+                        new URLSearchParams({
+                            _page: 1,
+                            _per_page: 1,
+                            ...(query && { q: JSON.stringify(query) }),
+                        }),
+                }),
+        };
+    }
+}
+
+export default ILLAPIClient;

koha-tmpl/intranet-tmpl/prog/js/vue/routes/ill.js
@@ -1,0 +1,130 @@
+import { markRaw } from "vue";
+
+import Home from "../components/ILL/Home.vue";
+import ProgressRequest from "../components/ILL/ProgressRequest.vue";
+import ResourceWrapper from "../components/ResourceWrapper.vue";
+
+import { $__ } from "@koha-vue/i18n";
+
+export const routes = [
+    {
+        path: "/cgi-bin/koha/ill/ill.pl",
+        redirect: "/cgi-bin/koha/ill/home",
+        is_default: true,
+        is_base: true,
+        title: $__("Interlibrary loans"),
+        children: [
+            {
+                path: "/cgi-bin/koha/ill/home",
+                name: "Home",
+                component: markRaw(Home),
+                title: $__("Home"),
+                icon: "fa fa-home",
+            },
+            {
+                path: "/cgi-bin/koha/ill",
+                title: $__("Borrowing"),
+                icon: "fa fa-arrow-circle-down",
+                disabled: true,
+                children: [
+                    {
+                        path: "/cgi-bin/koha/ill/ill-requests.pl",
+                        title: $__("Requests"),
+                        icon: "fa fa-download",
+                        is_external: true,
+                        is_navigation_item: true,
+                    },
+                    {
+                        path: "/cgi-bin/koha/ill/ill-requests.pl?method=batch_list",
+                        title: $__("Batches"),
+                        icon: "fa fa-clone",
+                        is_external: true,
+                        is_navigation_item: true,
+                    },
+                ],
+            },
+            {
+                path: "/cgi-bin/koha/ill",
+                title: $__("Lending"),
+                icon: "fa fa-arrow-circle-up",
+                disabled: true,
+                children: [
+                    {
+                        path: "/cgi-bin/koha/ill/iso18626_requests",
+                        title: $__("Supplying ILLs"),
+                        icon: "fa fa-upload",
+                        is_end_node: true,
+                        resource: "ILL/SupplyingResource.vue",
+                        children: [
+                            {
+                                path: "",
+                                name: "SupplyingList",
+                                component: markRaw(ResourceWrapper),
+                            },
+                            {
+                                path: ":iso18626_request_id",
+                                name: "SupplyingShow",
+                                component: markRaw(ResourceWrapper),
+                                title: "{iso18626_request_id}",
+                            },
+                            {
+                                path: "progress-request/:iso18626_request_id",
+                                name: "ProgressRequest",
+                                component: markRaw(ProgressRequest),
+                                title: $__(
+                                    "Progress request {iso18626_request_id}"
+                                ),
+                            },
+                        ],
+                    },
+                    {
+                        path: "/cgi-bin/koha/ill/iso18626_requesting_agencies",
+                        title: $__("Requesting Agencies"),
+                        icon: "fa fa-building-columns",
+                        is_end_node: true,
+                        resource: "ILL/RequestingAgencyResource.vue",
+                        children: [
+                            {
+                                path: "",
+                                name: "RequestingAgenciesList",
+                                component: markRaw(ResourceWrapper),
+                            },
+                            {
+                                path: ":iso18626_requesting_agency_id",
+                                name: "RequestingAgenciesShow",
+                                component: markRaw(ResourceWrapper),
+                                title: "{name}",
+                            },
+                            {
+                                path: "add",
+                                name: "RequestingAgenciesFormAdd",
+                                component: markRaw(ResourceWrapper),
+                                title: $__("Add requesting agency"),
+                            },
+                            {
+                                path: "edit/:iso18626_requesting_agency_id",
+                                name: "RequestingAgenciesFormAddEdit",
+                                component: markRaw(ResourceWrapper),
+                                title: "{name}",
+                                breadcrumbFormat: ({
+                                    match,
+                                    params,
+                                    query,
+                                }) => {
+                                    match.name = "RequestingAgenciesShow";
+                                    return match;
+                                },
+                                additionalBreadcrumbs: [
+                                    {
+                                        title: $__("Modify requesting agency"),
+                                        disabled: true,
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+];