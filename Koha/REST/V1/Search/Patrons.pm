be3effae9bca73c6858f66a76c8453bd5b1f6fa3
Tomás Cohen Arazi <tomascohen@theke.io>
2026-05-15 17:27:31 -0300

    Bug XXX: Add /search/patrons API endpoint scaffold
    Adds a dedicated search namespace under /api/v1/search/ for
    Elasticsearch-powered search endpoints.
    
    Endpoints:
      GET /search/patrons - Full-text patron search with sorting
      GET /search/patrons/autocomplete - Completion suggester
    
    Both require borrowers:list_borrowers permission and return 400
    if ElasticsearchPatronSearch syspref is not enabled.
    
    Controller methods are stubbed with TODO markers for the ES
    query implementation.
    
    Assisted-by: Sonnet 4.6 (Anthropic)

---
+++ Koha/REST/V1/Search/Patrons.pm
@@ -0,0 +1,107 @@
+package Koha::REST::V1::Search::Patrons;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <https://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use C4::Context;
+use Koha::Patrons;
+
+use Try::Tiny qw( catch try );
+
+=head1 NAME
+
+Koha::REST::V1::Search::Patrons
+
+=head1 API
+
+=head2 Methods
+
+=head3 search
+
+Controller function for searching patrons via Elasticsearch
+
+=cut
+
+sub search {
+    my $c = shift->openapi->valid_input or return;
+
+    return try {
+        my $q        = $c->param('q');
+        my $fields   = $c->param('fields');
+        my $order_by = $c->param('_order_by');
+        my $page     = $c->param('_page') // 1;
+        my $per_page = $c->param('_per_page') // 20;
+
+        unless ( C4::Context->preference('ElasticsearchPatronSearch') ) {
+            return $c->render(
+                status  => 400,
+                openapi => { error => "ElasticsearchPatronSearch is not enabled" },
+            );
+        }
+
+        # TODO: Implement ES search
+        # 1. Resolve searchable fields based on caller's library
+        # 2. Build multi_match query
+        # 3. Apply sorting
+        # 4. Execute search
+        # 5. Hydrate results from DB
+
+        return $c->render(
+            status  => 200,
+            openapi => { total => 0, hits => [] },
+        );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 autocomplete
+
+Controller function for patron autocomplete via Elasticsearch completion suggester
+
+=cut
+
+sub autocomplete {
+    my $c = shift->openapi->valid_input or return;
+
+    return try {
+        my $q        = $c->param('q');
+        my $per_page = $c->param('_per_page') // 10;
+
+        unless ( C4::Context->preference('ElasticsearchPatronSearch') ) {
+            return $c->render(
+                status  => 400,
+                openapi => { error => "ElasticsearchPatronSearch is not enabled" },
+            );
+        }
+
+        # TODO: Implement ES completion suggester
+        # 1. Build suggest query
+        # 2. Return lightweight patron summaries
+
+        return $c->render(
+            status  => 200,
+            openapi => [],
+        );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+1;
