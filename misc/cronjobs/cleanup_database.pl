9e7b2bc0f3d4149a11af9271267bc046dbacfb17
Nick Clemens <nick@bywatersolutions.com>
2026-05-28 15:03:21 -0300

    Bug XXX: Remove redundant manual cronlogaction calls from scripts
    Remove start/end cronlogaction calls and unused C4::Log imports
    from scripts that use Koha::Script, as these are now handled
    automatically by the INIT/END blocks.
    
    Scripts with mid-execution informational logging (plugins_nightly,
    harvest_oai, fines, process_message_queue) retain their C4::Log
    import and non-start/end cronlogaction calls.
    
    Signed-off-by: Tomás Cohen Arazi <tomascohen@theke.io>

--- misc/cronjobs/cleanup_database.pl
+++ misc/cronjobs/cleanup_database.pl
@@ -38,7 +38,6 @@ use Koha::Script -cron;
 
 use C4::Accounts qw( purge_zero_balance_fees );
 use C4::Context;
-use C4::Log qw( cronlogaction );
 use C4::Search::History;
 use C4::Search;
 use Koha::BackgroundJobs;
@@ -178,10 +177,6 @@ my $jobs_days;
 my @jobs_types;
 my $reports;
 my $edifact_msg_days;
-
-my $command_line_options = join( " ", @ARGV );
-cronlogaction( { info => $command_line_options } );
-
 GetOptions(
     'h|help'                     => \$help,
     'confirm'                    => \$confirm,
@@ -823,8 +818,6 @@ if ($edifact_msg_days) {
             : sprintf( "%d EDIFACT messages would have been removed", $count );
     }
 }
-
-cronlogaction( { action => 'End', info => "COMPLETED" } );
 
 exit(0);
 
